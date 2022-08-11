318405d4e397bc0d6b104c6aea6ad7918b04a2aa
Ievgenii Makhonko <yobaua@gmail.com>
2022-08-11 13:45:17 +0800

    added manageMovie method

--- Main.java
+++ Main.java
@@ -17,6 +17,7 @@ public class Main {
         } finally {
             System.out.println("MOVIES LOADED");
             System.out.println(store);
+            manageMovies();
         }
     }
 
@@ -30,7 +31,32 @@ public class Main {
      * •        case c: ask for the name and return.
      * • 3. call close() from the Scanner object.
      */
-
+public static void manageMovies(){
+    Scanner scanner = new Scanner(System.in);
+    while (true){
+        System.out.print("Would you like to\n\ta) purchase\n\tb) rent\n\tc) return\n");
+        String response = scanner.nextLine();
+        switch (response) {
+            case "a" -> {
+                System.out.print("\tEnter the name of the movie you want to sell: ");
+                String name = scanner.nextLine();
+                store.action(name, "sellMovie");
+                continue;
+            }
+            case "b" -> {
+                System.out.print("\tEnter the name of the movie you want to rent: ");
+                String name = scanner.nextLine();
+                store.action(name, "rentMovie");
+            }
+            case "c" -> {
+                System.out.print("\tEnter the name of the movie you want to return: ");
+                String name = scanner.nextLine();
+                store.action(name, "returnMovie");
+            }
+        }
+        System.out.println("\n\nUPDATED MOVIES\n\n" + store);
+    }
+}
 
     /**
      * Name: loadMovies
@@ -44,7 +70,6 @@ public class Main {
     public static void loadMovies(String fileName) throws FileNotFoundException {
         FileInputStream fileInputStream = new FileInputStream(fileName);
         Scanner scanner = new Scanner(fileInputStream);
-        scanner.useDelimiter("--");
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] words = line.split("--");
